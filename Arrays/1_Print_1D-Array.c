03b4cab521d0ae729408722ccde050a4a3a16620
AjayBhargavBorra1997 <borraajaybhargav@gmail.com>
2025-06-30 22:50:09 -0700

    Create 1_Print_1D-Array.c

---
+++ Arrays/1_Print_1D-Array.c
@@ -0,0 +1,29 @@
+// Print a Array
+
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int arr[5]={1,2,3,4,5};
+    int i;
+    for(i=0;i<5;i++)
+    {
+        printf("arr[%d] = %d \n",i,arr[i]);
+    }
+    
+    return 0;
+}
+
+
+
+/*
+
+Output:
+
+arr[0] = 1 
+arr[1] = 2 
+arr[2] = 3 
+arr[3] = 4 
+arr[4] = 5 
+
+*/
